2fddf92a686ec5c852f881bb8140550183b81e42
Ishika Jain <140393692+Ishika0123@users.noreply.github.com>
2024-04-05 00:11:34 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/Panorama-Blog-Header_1.gif"
           alt="product_image1"
         />
       </a>
